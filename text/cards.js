af6d67bb5de00b6055040d61c123aabc2a0b024c
nitink-ps1 <konidalanithin@gmail.com>
2020-06-14 09:46:26 +0530

    editable

---
+++ text/cards.js
@@ -0,0 +1,94 @@
+var card1 = `
+<h1 >History of Nuclear Power</h1>
+
+    <br>The History of Nuclear Energy begins on November 8, 1895 when Wilhelm Röntgen accidently discovers X-rays. 
+    Once Röntgen had discovered X-rays, scientists all over Europe began to experiment in the field. 
+    Most notable is Becquerel who, in 1896 and 1897 discovered that: Uranium is a strong source of radiation.<br><br>
+    
+    In 1932 physicist Ernest Rutherford discovered that when lithium atoms were "split" by protons from a proton accelerator, 
+    immense amounts of energy were released in accordance with the principle of mass–energy equivalence.<br><br>
+    
+    In 1934 Enrico Fermi showed that neutrons could split many different types of atoms and yield results lighter than their original reactants.
+    This was further developed and  Otto Hahn and Fritz Strassman proved that when the atoms are split,
+        energy is in turn released; thereby discovering nuclear fission.<br><br>
+
+    Further more, the discovery of chain reaction lead to the creation of the first nuclear reactor in the year 1942, and more and more reactors have
+    been built since then.
+`
+
+var card2 = `
+<h1>How is Nuclear energy generated?</h1>
+
+<br>A nuclear reactor produces electricity in much the same way other power plants do. The chain reaction produces energy in 
+    compliance with Mass-Energy Equivalance, which turns water into steam. The pressure of the steam turns a generator, which produces electricity. 
+    The difference is in how the heat is created. Power plants that run on fossil fuels burn coal, oil or natural gas to generate heat. 
+    In a nuclear energy plant, heat is produced from splitting atoms – a process called nuclear fission<br><br>
+
+    1.) Nuclear reactor creates heat that is used to make steam<br>
+    2.) The steam turns a turbine connected to an electromagnet, called a generator<br>
+    3.) The generator produces electricity<br><br>
+
+    For more than 60 years, nuclear energy has provided the world with reliable electricity. 
+    Today, more than 400 reactors are operating in more than 30 countries. These plants generate around 
+    10 percent of the world’s electricity without emitting greenhouse gases. More nations are exploring 
+    the use of nuclear energy, particularly as electricity demand increases and concerns about climate change rise. 
+`
+
+var card3 = `
+<h1>Nuclear Fission and Fusion</h1>
+
+    <br>Nuclear fission takes place when a large, somewhat unstable isotope (atoms with the same number of protons but different number of neutrons)
+    is bombarded by high-speed particles, usually neutrons. These neutrons are accelerated and then slammed into the unstable isotope, 
+    causing it to fission, or break into smaller particles. During the process, a neutron is accelerated and strikes the target nucleus, 
+    which in the majority of nuclear power reactors today is Uranium-235. This splits the target nucleus and breaks it down into two smaller 
+    isotopes (the fission products), three high-speed neutrons, and a large amount of energy.<br><br>
+
+    The word fusion means "a merging of separate elements into a unified whole". Nuclear fusion refers to the "union of atomic nuclei to form 
+    heavier nuclei resulting in the release of enormous amounts of energy". Fusion takes place when 
+    two low-mass isotopes, typically isotopes of hydrogen, unite under conditions of extreme pressure and temperature.
+`
+
+var card4 = `
+<h1>Working of Nuclear Reactors</h1>
+
+<br><h5>There are primarily two types of light-water reactors :</h5> <br>
+    <em>Pressurized water reactors</em><br>
+    These reactors pump water into the reactor core under high pressure to prevent the water from boiling. The water in the core is heated 
+    by nuclear fission and then pumped into tubes inside a heat exchanger. Those tubes heat a separate water source to create steam. The 
+    steam then turns an electric generator to produce electricity. The core water cycles back to the reactor to be reheated and the process 
+    is repeated.<br><br>
+    <em>Boiling water reactors</em><br>
+    BWRs heat water and produce steam directly inside the reactor vessel. Water is pumped up through the reactor core and heated by fission. 
+    Pipes then feed the steam directly to a turbine to produce electricity. The unused steam is then condensed back to water and reused in 
+    the heating process.<br>
+`
+
+var card5 = `
+<h1>Safety and Hazards</h1>
+<br><h5>Nuclear reactors have three unique characteristics that affect their safety, as compared to other power plants :</h5><br> 
+
+    <ul>
+    <li style="list-style-type: circle;">Firstly, intensely radioactive materials are present in a nuclear reactor. Their release to the environment could be hazardous.<br></li>
+    <br>
+    <li style="list-style-type: circle;">Secondly, the fission products, which make up most of the intensely radioactive substances in the reactor, continue to generate 
+    a significant amount of decay heat even after the fission chain reaction has stopped. If the heat cannot be removed from the 
+    reactor, the fuel rods may overheat and release radioactive materials.</li>
+    <br>
+    <li style="list-style-type: circle;">Thirdly, a criticality accident (a rapid increase of the reactor power) is possible in certain reactor designs if the chain 
+    reaction cannot be controlled. These three characteristics have to be taken into account when designing nuclear reactors.</li>
+    </ul>
+`
+
+var card6 = `
+<h1>Future of Nuclear Energy</h1>
+    <br>
+    The future of nuclear power varies greatly between countries, depending on government policies. Some countries, most notably, Germany, 
+    have adopted policies of nuclear power phase-out. At the same time, some Asian countries, such as China and India, have committed 
+    to rapid expansion of nuclear power. Many other countries, such as the United Kingdom and the United States, have policies in between. 
+    Japan generated about 30% of its electricity from nuclear power before the Fukushima accident. In 2015 the Japanese government committed to 
+    the aim of restarting its fleet of 40 reactors by 2030 after safety upgrades, and to finish the construction of the Generation III Ōma Nuclear 
+    Power Plant. This would mean that approximately 20% of electricity would come from nuclear power by 2030. As of 2018, some reactors have 
+    restarted commercial operation following inspections and upgrades with new regulations. While South Korea has a large nuclear power 
+    industry, the new government in 2017, influenced by a vocal anti-nuclear movement, committed to halting nuclear development after the 
+    completion of the facilities presently under construction.
+`
---
+++ text/main.js
@@ -0,0 +1,6 @@
+var title = ` TNSTC&nbsp;<em> Nuclear Power</em> `
+
+var heading1 = `Practise School-1 &nbsp;: &nbsp;TNSTC Digital Content - Nuclear Power `
+
+var heading2 = `14% of world's electricity is now <em>NUCLEAR</em>`
+
