506035cb39c2d88cb4c81ca537168b2dc3f019bc
nitink-ps1 <konidalanithin@gmail.com>
2020-06-17 01:46:11 +0530

    india

---
+++ india/text.js
@@ -0,0 +1,22 @@
+//JS variables to hold text
+
+var option1 =`
+In  India,  nuclear  energy  development  began  with  the  objectives  of  peaceful  uses  of  atomic  energy  in  improving  
+the  quality  of  life  of  the  people  and  to  achieve  self-reliance in meeting the energy needs.
+`
+var option2 =`
+Nuclear power is the fifth-largest source of electricity in India after coal, gas, hydroelectricity and wind power. As of March 2018, 
+India has 22 nuclear reactors in operation in 7 nuclear power plants.
+`
+var option3 =`
+India is committing to reduce the economy’s carbon intensity and increase clean energy capacity. 
+Nuclear energy—with its massive potential—will have to play a key role in the country’s future energy mix.
+`
+var card1 =`
+
+`
+var card2 =`
+
+`
+var card3 =`
+`
