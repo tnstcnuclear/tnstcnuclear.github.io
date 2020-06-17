226ca20b725073f4d7358d3647e0d48eb892dfc5
nitink-ps1 <konidalanithin@gmail.com>
2020-06-17 23:05:01 +0530

    india

--- india/text.js
+++ india/text.js
@@ -13,10 +13,43 @@ India is committing to reduce the economy’s carbon intensity and increase clea
 Nuclear energy—with its massive potential—will have to play a key role in the country’s future energy mix.
 `
 var card1 =`
+<h2>History of Nuclear Power in India</h2><br>
+Shortly after India’s independence in 1947, Dr Homi Bhabha wrote to Prime Minister Jawaharlal Nehru, arguing that “within the next couple of decades, 
+atomic energy would play an important part in the economy and the industry of countries and that, if India did not wish to fall even further 
+behind industrially advanced countries of the world, it would be necessary to develop this branch of science.”
+In 1954, Bhabha founded a nuclear research center at Bombay which was later renamed the Bhabha Atomic Research Centre (BARC).<br><br>
 
+India's first prime minister, Jawaharlal Nehru, delivered a speech in 1948 to the General Assembly of India strongly advocating nuclear 
+energy. The result was the establishment of the Atomic Energy Commission, and later to the creation of the Department of Atomic Energy (DAE). 
+The DAE, after developing the first operational research reactor in 1956, then proceeded to construct and complete India's first commercial reactor, 
+Tarapur Atomic Power Station, in 1969. From that point, Indian nuclear program has evolved a lot and as of 2018, 3% of electricity in India comes from nuclear.
 `
 var card2 =`
+<h2>Current situation of Nuclear Power in India</h2><br>
+Nuclear power is the fifth-largest source of electricity in India after coal, gas, hydroelectricity and wind power. As of March 2018, India has 
+22 nuclear reactors in operation in 7 nuclear power plants, with a total installed capacity of 6,780 MW. Nuclear power produced a total of 35 TWh 
+and supplied 3.22% of Indian electricity in 2017. The folowing are the seven operational power plants operated by Nuclear Power Corporation of 
+India Limited (NPCIL) : <br> <br>
 
+1. Tarapur, Maharashtra <br>
+2. Rawatbhata, Rajasthan <br>
+3. Kudankulam, Tamil Nadu <br>
+4. Kaiga, Karnataka <br>
+5. Kakrapar, Gujarat <br>
+6. Kalpakkam, Tamil Nadu <br>
+7. Narora, UP <br>
+<br>Seven more reactors are under construction with a combined generation capacity of 4,300 MW. 
 `
 var card3 =`
+<h2>Future of Nuclear Power in India</h2><br>
+India plans to build 21 new nuclear power reactors - including 10 indigenously designed pressurised heavy water reactors (PHWRs) - 
+with a combined generating capacity of 15,700 MWe into operation by 2031, the DAE announced.
+<br><br>The most important source of energy for India in the coming decades is nuclear power, given its huge potential for growth, 
+emission-free nature and consistent nature of production. A significant expansion of nuclear power can both enable the connectivity 
+of millions of Indians who currently lack access to the power grid and help it contribute to global efforts to tackle climate change 
+by curbing its total carbon emissions.
+<br><br>
+The Indian government is committed to growing its nuclear power capacity as part of its infrastructure development programme, and has 
+seven units currently under construction. These are: four indigenously designed pressurised heavy water reactors (PWRs), two each at 
+Kakrapar and Rajasthan; two Russian-designed VVER PWRs at Kudankulam; and an indigenously designed prototype fast breeder reactor at Kalpakkam.
 `
