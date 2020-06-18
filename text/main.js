31a01d32905e8450da88a29c0411a3f3a95c36ad
nitink-ps1 <konidalanithin@gmail.com>
2020-06-18 11:28:18 +0530

    heading change

--- text/main.js
+++ text/main.js
@@ -1,6 +1,6 @@
 var title = ` TNSTC&nbsp;<em> Nuclear Power</em> `
 
-var heading1 = `Practise School-1 &nbsp;: &nbsp;TNSTC Digital Content - Nuclear Power `
+var heading1 = `TNSTC Digital Content - Nuclear Power `
 
-var heading2 = `14% of world's electricity is now <em>NUCLEAR</em>`
+var heading2 = `More than 10% of the world's electricity is now <em>NUCLEAR</em>`
 
